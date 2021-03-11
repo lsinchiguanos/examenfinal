319b97c8d28c9f21ff9e9e74ae85ccf390617e4f
lsinchiguanos <leslie.sinchiguano2015@uteq.edu.ec>
2021-03-11 17:28:07 -0500

    Por joto XD se me olvido colocar la s en http lol

--- app/src/main/java/uteq/student/project/examenfinal/MainActivity.java
+++ app/src/main/java/uteq/student/project/examenfinal/MainActivity.java
@@ -1,14 +1,76 @@
 package uteq.student.project.examenfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import uteq.student.project.examenfinal.models.Journals;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Journals journals;
+    private ArrayList<Journals> journalsArrayList;
+    private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestQueue = Volley.newRequestQueue(this);
+        getDataWebService();
     }
+
+    private void getDataWebService() {
+        journalsArrayList = new ArrayList<>();
+        String url = "https://revistas.uteq.edu.ec/ws/journals.php";
+        JsonArrayRequest jsonObjectRequest = new JsonArrayRequest(
+                Request.Method.POST,
+                url,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        int size = response.length();
+                        for (int i = 0; i < size; i++){
+                            try {
+                                JSONObject jsonObject = new JSONObject(response.get(i).toString());
+                                journals = new Journals();
+                                journals.setJournal_id(jsonObject.getString("journal_id"));
+                                journals.setPortada(jsonObject.getString("portada"));
+                                journals.setAbbreviation(jsonObject.getString("abbreviation"));
+                                journals.setName(jsonObject.getString("name"));
+                                journalsArrayList.add(journals);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("HttpClient", "error: " + error.toString());
+                    }
+                }
+        );
+        requestQueue.add(jsonObjectRequest);
+    }
+
 }
